a7cb617ee4522865ea473bebb55873b7cd55002f
SupianIDz <supianidz@gmail.com>
2020-08-03 09:02:35 +0800

    UI Improvement

--- resources/views/selector.blade.php
+++ resources/views/selector.blade.php
@@ -1,31 +1,30 @@
-<link rel="stylesheet" href="/vendor/octopyid/sudo/sudo.css?{{ microtime() }}">
+<link rel="stylesheet" href="/vendor/octopyid/sudo/sudo.min.css">
 <link rel="stylesheet" href="/vendor/octopyid/sudo/tail.min.css">
 
 <div class="octopyid-sudo">
-    <div class="octopyid-sudo-btn {{ $hasSudoed ? 'octopyid-sudo-btn-has-sudoed' : '' }}" style="position: relative;"
+    <div class="octopyid-sudo-btn {{ $hasSudoed ? 'octopyid-sudo-btn-has-sudoed' : '' }}"
+         style="position: relative;"
          id="octopyid-sudo-toggle">
         <img src="/vendor/octopyid/sudo/sudo.svg">
     </div>
 
-    <div class="octopyid-sudo-interface {{ $hasSudoed ? 'octopyid-sudo-interface-has-sudoed' : '' }} hidden"
+    <div class="octopyid-sudo-interface octopyid-sudo-interface-has-sudoed hidden"
          id="octopyid-sudo-interface">
         @if ($hasSudoed)
-            @if ($originalUser)
-                <div class="octopyid-sudo-info-line">
-                    <table>
-                        <tr>
-                            <td class="octopyid-sudo-font-bold">Sign In As</td>
-                            <td class="octopyid-sudo-font-bold">:</td>
-                            <td>{{ $currentUser->name }}</td>
-                        </tr>
-                        <tr>
-                            <td class="octopyid-sudo-font-bold">My Account</td>
-                            <td class="octopyid-sudo-font-bold">:</td>
-                            <td>{{ $originalUser->name }}</td>
-                        </tr>
-                    </table>
-                </div>
-            @endif
+            <div class="octopyid-sudo-info-line">
+                <table>
+                    <tr>
+                        <td class="octopyid-sudo-font-bold">Sign In As</td>
+                        <td class="octopyid-sudo-font-bold">:</td>
+                        <td>{{ $currentUser->name }}</td>
+                    </tr>
+                    <tr>
+                        <td class="octopyid-sudo-font-bold">My Account</td>
+                        <td class="octopyid-sudo-font-bold">:</td>
+                        <td>{{ $originalUser->name }}</td>
+                    </tr>
+                </table>
+            </div>
         @endif
         <form method="POST" action="{{ route('sudo.signin') }}">
             {!! csrf_field() !!}
@@ -42,15 +41,16 @@
 
 <script type="text/javascript" src="/vendor/octopyid/sudo/tail.min.js"></script>
 <script type="text/javascript">
+    const button = document.getElementById('octopyid-sudo-toggle');
+    const element = document.getElementById('octopyid-sudo-interface');
+
+    button.addEventListener('click', event => element.classList.toggle('hidden'));
+
     document.addEventListener('DOMContentLoaded', () => {
         tail.select(document.getElementById('octopy-sudo-select'), {
             search: true,
             width: '100%'
         });
     });
-
-    const button = document.getElementById('octopyid-sudo-toggle');
-    const element = document.getElementById('octopyid-sudo-interface');
-
-    button.addEventListener('click', event => element.classList.toggle('hidden'));
 </script>
+
