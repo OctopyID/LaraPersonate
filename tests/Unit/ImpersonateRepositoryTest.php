1e25474296d0424fd3f482a25d7c9eef52e030fb
SupianIDz <supianidz@gmail.com>
2022-06-08 21:31:43 +0800

    Add More Unit Tests

---
+++ tests/Unit/ImpersonateRepositoryTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Octopy\Impersonate\Tests\Unit;
+
+use Octopy\Impersonate\Impersonate;
+use Octopy\Impersonate\ImpersonateRepository;
+use Octopy\Impersonate\Tests\Models\User;
+use Octopy\Impersonate\Tests\TestCase;
+
+class ImpersonateRepositoryTest extends TestCase
+{
+    /**
+     * @var Impersonate
+     */
+    protected Impersonate $impersonate;
+
+    /**
+     * @return void
+     */
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        $this->impersonate = $this->app->make('impersonate');
+    }
+
+    /**
+     * @return void
+     */
+    public function testItGetImpersonatorInStorage() : void
+    {
+        $foo = User::create([
+            'name'  => 'Foo',
+            'email' => 'foo@bar.baz',
+        ]);
+
+        $bar = User::create([
+            'name'  => 'Bar',
+            'email' => 'bar@baz.qux',
+        ]);
+
+        $this->actingAs($foo);
+
+        $foo->impersonate($bar);
+
+        $repository = new ImpersonateRepository($this->impersonate);
+
+        $this->assertEquals($foo->refresh()->toArray(), $repository->getImpersonatorInStorage()->toArray());
+    }
+
+    /**
+     * @return void
+     */
+    public function testItGetImpersonatedInStorage() : void
+    {
+        $foo = User::create([
+            'name'  => 'Foo',
+            'email' => 'foo@bar.baz',
+        ]);
+
+        $bar = User::create([
+            'name'  => 'Bar',
+            'email' => 'bar@baz.qux',
+        ]);
+
+        $this->actingAs($foo);
+
+        $foo->impersonate($bar);
+
+        $repository = new ImpersonateRepository($this->impersonate);
+
+        $this->assertEquals($bar->refresh()->toArray(), $repository->getImpersonatedInStorage()->toArray());
+    }
+}
